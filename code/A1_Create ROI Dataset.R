046e9f1200a12afb9e2f03b771d2e320dd4b3ffe
Matt Martinez <mattmtz@gmail.com>
2024-06-04 17:15:55 -0400

    initial work to create consolidated dataset - still in progress

---
+++ code/A1_Create ROI Dataset.R
@@ -0,0 +1,181 @@
+###---------------------------------------------------------------------------#
+### Project : COLLEGE SCORECARD ROI
+### Purpose : CALCULATE NPVS
+### Author  : MATT MARTINEZ
+### Audited : NO
+###---------------------------------------------------------------------------#
+
+rm(list = ls())
+options(scipen = 999)
+
+# Set working directory
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd("..")
+
+# Load packages
+library(data.table)
+library(tidyverse)
+library(magrittr)
+library(lubridate)
+library(stringr)
+library(janitor)
+library(openxlsx)
+library(FinCal)
+
+#### (0) SETUP ----------------------------------------------------------------
+
+# Define constants
+r = 0.02
+FEDRATE = 0.0499
+
+# Identify years with earnings data
+KEYYRS <- c(2003,2005,2007,2009,2011:2014,2018,2019)
+
+# Variables that need deflating
+DEFL_VARS <- c(paste0("md_earn_wne_p",c(6,8,10)), "net_price")
+
+# List key files
+KEYFILES <- c(paste0("MERGED",KEYYRS,"_", 
+                     str_sub(as.character(KEYYRS+1),-2),"_PP.csv"),
+              "Most-Recent-Cohorts-Institution.csv")
+
+# Inflation data
+cps <- read.xlsx("input/r-cpi-u-rs-alllessfe.xlsx", sheet = 1,
+                 startRow = 6) %>%
+  clean_names() %>%
+  select(year, avg) %>%
+  rename(cps_yr = year,
+         cps_deflator = avg)
+
+# Cohort crosswalk
+cohorts <- fread("input/cohort_xwalk.csv")
+
+# Load ccbasic crosswalk
+xwalk <- fread("input/ccbasic_xwalk.csv")
+
+#### (1) COMBINE INDIVIDUAL FILES ---------------------------------------------
+
+# Identify key variables
+IDVARS <- c("UNITID", "OPEID", "OPEID6", "INSTNM", "CONTROL", "STABBR",
+            "ST_FIPS", "REGION", "PREDDEG", "CCBASIC","LOCALE", "LOCALE2", 
+            "ICLEVEL", "CCUGPROF", "CCSIZSET", "MENONLY", "WOMENONLY", 
+            "AGE_ENTRY", "OPENADMP", "RELAFFIL","NUMBRANCH", "UGDS",
+            "HBCU", "PBI", "ANNHI", "TRIBAL", "AANAPII", "HSI", "NANTI", 
+            "ADM_RATE", "MAIN", "PCTPELL", "UGDS_MEN", "UGDS_WOMEN")
+
+DEBTVARS <- c("NPT4_PUB", "NPT4_PRIV", "NPT4_PROG", "NPT4_OTHER", "DEBT_MDN",
+              "RPY_7YR_RT")
+
+EARNVARS <- c("MD_EARN_WNE_P6" ,"MD_EARN_WNE_P8", "MD_EARN_WNE_P10",
+              "GT_THRESHOLD_P10")
+
+GRADVARS <- c("C150_L4_POOLED_SUPP", "C150_4_POOLED_SUPP")
+
+ALLVARS <- c(IDVARS, DEBTVARS, EARNVARS, GRADVARS)
+# Container dataset
+allyrs <- data.frame()
+
+# Function for processing individual datasets
+CLN_YRS <- function(x) {
+  
+  # Download file
+  individ_yr <- fread(paste0("../raw_data/",KEYFILES[x])) %>%
+    select(any_of(ALLVARS)) %>%
+    clean_names() %>%
+    # Add years & file name
+    mutate(year = paste0(KEYYRS[x],"_",str_sub(as.character(KEYYRS[x]+1),-2)),
+           file_name = KEYFILES[x]) %>%
+    mutate(across(everything(), as.character))
+  
+  # Add to overall dataset
+  allyrs <- bind_rows(allyrs, individ_yr)
+  
+  return(allyrs)
+}
+
+
+# Select relevant variables
+raw_combination <- lapply(1:length(KEYFILES), CLN_YRS) %>% bind_rows() 
+
+# Find which files have missing data
+filecounts <- raw_combination %>% group_by(file_name) %>% count()
+
+missingflags <- raw_combination %>%
+  group_by(file_name) %>%
+  summarise(across(tolower(ALLVARS),  ~sum(is.na(.)))) %>%
+  ungroup() %>%
+  left_join(filecounts) %>%
+  select(file_name, n, everything()) %>%
+  mutate(across(tolower(ALLVARS[1]):tolower(ALLVARS[length(ALLVARS)]),
+                ~ ifelse(n-.x == 0, "NO DATA", ""))) %>%
+  select(-c(file_name, n))
+
+
+missing_data_summary <- as.data.frame(t(missingflags))
+names(missing_data_summary) <- KEYFILES
+fwrite(missing_data_summary, "output/missing_variables_summary.csv",
+       row.names = T)
+
+
+
+################# TO INCORPORATE LATER ########################################
+
+#### (2) CLEAN DATASET --------------------------------------------------------
+
+# De-code relevant variables
+decoded_data <- filtered_data %>%
+  left_join(xwalk) %>%
+  select(-ccbasic) %>%
+  rename(ccbasic = cln_ccbasic) %>%
+  mutate(
+    debt_mdn = as.numeric(debt_mdn),
+    iclevel = factor(iclevel, levels = c(1,2,3),
+                     labels=c("4-year","2-year","Less than 2-year")),
+    control = factor(control, levels = c(1,2,3), 
+                     labels=c("Public","Private nonprofit",
+                              "Private for-profit")),
+    preddeg = factor(preddeg, levels = seq(0,4), 
+                     labels = c("Not classified", "Certificate", "Associate's",
+                                "Bachelor's", "Graduate")),
+    openadmp = case_when(
+      openadmp == 1 ~ "Yes",
+      openadmp == 2 ~ "No",
+      T             ~ "Does not admit first time students"),
+    adm_rate = ifelse(is.na(adm_rate) & openadmp == "Yes", 1, adm_rate),
+    grad_rate = ifelse(is.na(c150_l4_pooled_supp), c150_4_pooled_supp,
+                       c150_l4_pooled_supp),
+    region = factor(region, levels = seq(0,9),
+                    labels = c("U.S. Service Schools", 
+                               "New England (CT, ME, MA, NH, RI, VT)", 
+                               "Mid East (DE, DC, MD, NJ, NY, PA)", 
+                               "Great Lakes (IL, IN, MI, OH, WI)",
+                               "Plains (IA, KS, MN, MO, NE, ND, SD)",
+                               "Southeast (AL, AR, FL, GA, KY, LA, MS, NC, SC, TN, VA, WV)",
+                               "Southwest (AZ, NM, OK, TX)",
+                               "Rocky Mountains (CO, ID, MT, UT, WY)",
+                               "Far West (AK, CA, HI, NV, OR, WA)",
+                               "Outlying Areas (AS, FM, GU, MH, MP, PR, PW, VI)"))) %>%
+  mutate(grad_rate = ifelse(grad_rate == "PS", NA, grad_rate)) %>%
+  mutate(grad_rate = as.numeric(grad_rate)) %>%
+  droplevels()
+
+# Create enrollment categories, create flag for institutions reporting together
+# & drop graduate institutions
+cln_decoded_data <- decoded_data %>%
+  mutate(ugdssize = forcats::fct_na_value_to_level(
+    cut(ugds, breaks = c(0,250,500,1e7), 
+        labels=c("<=250","251-500",">500"), include.lowest=TRUE)),
+    report_as_group = ifelse(numbranch>1,1,0)) %>%
+  filter(!preddeg %in% c(0,4))
+
+# De-duplicate
+deduped_data <- cln_decoded_data %>%
+  distinct(instnm,stabbr, .keep_all = T)
+
+# Remove institutions with no program data
+program_data <- raw_fos %>%
+  clean_names() %>%
+  filter(!is.na(unitid), !is.na(opeid6)) %>%
+  distinct(unitid, opeid6)
+
+scorecard <- inner_join(deduped_data, program_data)
--- code/A1_Earnings by Cohort.R
+++ code/A1_Earnings by Cohort.R
@@ -23,19 +23,6 @@ library(openxlsx)
 
 #### (0) SETUP ----------------------------------------------------------------
 
-# Define Graph theme
-GraphTheme = theme_classic() + 
-  theme(
-    axis.line = element_line(linewidth = 1, colour = "black"),
-    panel.grid.major.y = element_line(linewidth = 0.4, color = "grey89"),
-    axis.title.x = element_text(size = 14),
-    axis.text.x = element_text(size = 12, color = "black"),
-    axis.title.y = element_text(size = 13),
-    axis.text.y = element_text(size = 12, color = "black"),
-    #legend.title = element_blank(),
-    legend.key.size = unit(0.8, 'cm'),
-    legend.background = element_blank())
-
 # List years of interest
 KEYYRS <- c(2003,2005,2007,2009,2011:2014,2018,2019)
 
@@ -123,46 +110,12 @@ full_data_cps <- full_data %>%
                 .names = "{.col}_23_dollars"))
 
 # export data
-#fwrite(full_data_cps, "intermediate/combined_scorecard_data.csv")
+fwrite(full_data_cps, "../intermediate/combined_scorecard_data.csv")
 
 # clean environment
 rm(combined_data, full_data); gc()
 
-#### (3) MEDIAN EARNINGS BY SURVEY YEAR ---------------------------------------
 
-# Set up data
-fig1 <- full_data_cps %>%
-  group_by(year) %>%
-  summarize('6-Year' = median(md_earn_wne_p6_23_dollars, na.rm=T)/1000,
-            '8-Year' = median(md_earn_wne_p8_23_dollars, na.rm = T)/1000,
-            '10-Year'= median(md_earn_wne_p10_23_dollars, na.rm = T)/1000) %>%
-  ungroup() %>%
-  pivot_longer(!year, names_to = "cohort", values_to = "earnings") %>%
-  mutate(cohort = factor(cohort, levels = c("6-Year", "8-Year", "10-Year"))) %>%
-  filter(!is.na(earnings))
-
-# Create plot
-ggplot(fig1, aes(x = year, y = earnings, group = cohort, color = cohort)) +
-  geom_point(size= 2.5) +
-  geom_line(linewidth = 1.2) +
-  scale_color_manual(values = c('cornflowerblue', 'darkorange1', 
-                                'forestgreen'),
-                     breaks = c("10-Year", "8-Year", "6-Year")) +
-  GraphTheme +
-  theme(legend.position = c(0.8,0.83),
-        legend.text = element_text(size = 13),
-        legend.title= element_text(size=14),
-        axis.text.x = element_text(angle = 45, vjust = 0.9, hjust = 1)) +
-  labs(color = "Years After Enrollment",
-       x = "College Scorecard Survey Vintage",
-       y = "Medians of Median Earnings ($, 2023)") +
-  scale_y_continuous(expand = c(0,0),
-                     limits = c(25, 52),
-                     labels = scales::dollar_format(prefix = "$", 
-                                                    suffix = "K"))
-# Save output
-ggsave("output/earnings_by_survey_yr.png", 
-       width = 3500, height = 2750, units = "px")
 
 #### (4) MEDIAN EARNINGS BY COHORT --------------------------------------------
 
